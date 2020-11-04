17ddacfa2c9942ff9932a477fcff75df7763e88b
Muhammad Alabadsa <moh22389@gmail.com>
2020-11-04 18:05:48 +0200

    Show Cart On DOM And Add Element And Delete Relates #4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,115 @@
-const products = [
-  {
-    id: "p1",
-    name: "T-shirt",
-    imageUrl: "https://via.placeholder.com/150/255595/808080",
-    details: "",
-    price: 25,
-    category: "cat_1",
-  },
-  {
-    id: "p2",
-    name: "Shouse",
-    imageUrl: "https://via.placeholder.com/150/255595/808080",
-    details: "",
-    price: 15,
-    category: "cat_1",
-  },
-  {
-    id: "p3",
-    name: "Toy",
-    imageUrl: "https://via.placeholder.com/150/255595/808080",
-    details: "",
-    price: 5,
-    category: "cat_2",
-  },
-];
+// const products = [
+//   {
+//     id: "p1",
+//     name: "T-shirt",
+//     imageUrl: "https://via.placeholder.com/150/255595/808080",
+//     details: "",
+//     price: 25,
+//     category: "cat_1",
+//   },
+//   {
+//     id: "p2",
+//     name: "Shouse",
+//     imageUrl: "https://via.placeholder.com/150/255595/808080",
+//     details: "",
+//     price: 15,
+//     category: "cat_1",
+//   },
+//   {
+//     id: "p3",
+//     name: "Toy",
+//     imageUrl: "https://via.placeholder.com/150/255595/808080",
+//     details: "",
+//     price: 5,
+//     category: "cat_2",
+//   },
+// ];
 
-const cart = [
-  {
-    id: "c1",
-    product: {
-      id: "p3",
-      name: "Toy",
-      imageUrl: "https://via.placeholder.com/150/255595/808080",
-      details: "",
-      price: 5,
-      category: "cat_2",
-    },
-    qty: 2,
-    total: 10,
-  },
-  {
-    id: "c2",
-    product: {
-      id: "p1",
-      name: "T-shirt",
-      imageUrl: "https://via.placeholder.com/150/255595/808080",
-      details: "",
-      price: 25,
-      category: "cat_1",
-    },
-    qty: 2,
-    total: 50,
-  },
-  {
-    id: "c3",
-    product: {
-      id: "p2",
-      name: "Shouse",
-      imageUrl: "https://via.placeholder.com/150/255595/808080",
-      details: "",
-      price: 15,
-      category: "cat_1",
-    },
-    qty: 3,
-    total: 45,
-  },
-];
+// const cart = [
+//   {
+//     id: "c1",
+//     product: {
+//       id: "p3",
+//       name: "Toy",
+//       imageUrl: "https://via.placeholder.com/150/255595/808080",
+//       details: "",
+//       price: 5,
+//       category: "cat_2",
+//     },
+//     qty: 2,
+//     total: 10,
+//   },
+//   {
+//     id: "c2",
+//     product: {
+//       id: "p1",
+//       name: "T-shirt",
+//       imageUrl: "https://via.placeholder.com/150/255595/808080",
+//       details: "",
+//       price: 25,
+//       category: "cat_1",
+//     },
+//     qty: 2,
+//     total: 50,
+//   },
+//   {
+//     id: "c3",
+//     product: {
+//       id: "p2",
+//       name: "Shouse",
+//       imageUrl: "https://via.placeholder.com/150/255595/808080",
+//       details: "",
+//       price: 15,
+//       category: "cat_1",
+//     },
+//     qty: 3,
+//     total: 45,
+//   },
+// ];
 
-const productSearch = function (text, products) {
+// if (!JSON.parse(localStorage.getItem("products"))) {
+  localStorage.setItem(
+    "products",
+    JSON.stringify([
+      {
+        id: "p1",
+        name: "T-shirt",
+        imageUrl: "https://via.placeholder.com/150/255595/808080",
+        details: "",
+        price: 25,
+        category: "cat_1",
+      },
+      {
+        id: "p2",
+        name: "Shouse",
+        imageUrl: "https://via.placeholder.com/150/255595/808080",
+        details: "",
+        price: 15,
+        category: "cat_1",
+      },
+      {
+        id: "p3",
+        name: "Toy",
+        imageUrl: "https://via.placeholder.com/150/255595/808080",
+        details: "",
+        price: 5,
+        category: "cat_2",
+      },
+    ])
+  );
+// }
+
+const products = JSON.parse(localStorage.getItem("products")) || [];
+
+if (!JSON.parse(localStorage.getItem("cart"))) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+const productSearch = function (text) {
   const textSearch = text.trim();
-  const productsArr = [...products];
+  const productsArr = JSON.parse(localStorage.getItem("products"));
   return productsArr.filter((item) => {
     return item.name.toLowerCase().includes(textSearch.toLowerCase());
   });
@@ -90,23 +130,29 @@ const filterProoduct = function (filteredBy) {
   });
 };
 
-const addToCart = function (product, myCart) {
+const addToCart = function (product) {
   const pId = product.id;
-  let newCart = [...myCart];
+  let newCart = JSON.parse(localStorage.getItem("cart"));
   for (let index = 0; index < newCart.length; index++) {
     if (newCart[index].product.id === pId) {
-      newCart[index].qty += 1;
+      ++newCart[index].qty;
       newCart[index].total = newCart[index].product.price * newCart[index].qty;
-      return newCart;
+      localStorage.setItem("cart", JSON.stringify([...newCart]));
+      return;
     }
   }
-
-  newCart.push({ id: "c5", qty: 1, total: product.price, product });
-  return newCart;
+  newCart.push({
+    id: "c-" + Math.floor(Math.random() * 1000),
+    qty: 1,
+    total: product.price,
+    product,
+  });
+  localStorage.setItem("cart", JSON.stringify(newCart));
 };
 
-const totalPrice = function (myCart) {
-  const newCart = [...myCart];
+const totalPrice = function () {
+  const newCart = JSON.parse(localStorage.getItem("cart"));
+  if (newCart.length === 0) return 0;
   let total = 0;
   newCart.forEach((element) => {
     total += element.total;
@@ -116,15 +162,15 @@ const totalPrice = function (myCart) {
   return total;
 };
 
-const deleteProductFromCart = (deleteProduct, oldCart) => {
-  const newCart = [...oldCart];
-
-  return newCart.filter((item) => {
-    if (item.product.id !== deleteProduct.id) {
-      console.log(item);
+const deleteProductFromCart = (productToDelete) => {
+  const newCart = JSON.parse(localStorage.getItem("cart"));
+  totalPrice(newCart);
+  const arrFiltered = newCart.filter((item) => {
+    if (item.product.id !== productToDelete.product.id) {
       return item;
     }
   });
+  localStorage.setItem("cart", JSON.stringify(arrFiltered));
 };
 
 module.exports = {
