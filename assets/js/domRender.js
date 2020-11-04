0d1052f8e67328f0de451e923788e451f7f7f69d
Muhammad Alabadsa <moh22389@gmail.com>
2020-11-04 09:13:43 +0200

    Render Prodcut Item On buyer Page

--- assets/js/domRender.js
+++ assets/js/domRender.js
@@ -1,5 +1,76 @@
-[{},{}].map(item => {
-    return `
-        
-    `
-})
+const products2 = [
+    {
+      id: "p1",
+      name: "T-shirt",
+      imageUrl: "https://via.placeholder.com/150/255595/808080",
+      details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      price: 25,
+    },
+    {
+      id: "p2",
+      name: "Shouse",
+      imageUrl: "https://via.placeholder.com/150/255595/808080",
+      details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      price: 15,
+    },
+    {
+      id: "p3",
+      name: "Toy",
+      imageUrl: "https://via.placeholder.com/150/255595/808080",
+      details: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      price: 5,
+    },
+  ];
+
+  let productContainer = document.getElementsByClassName("product-container")[0];
+
+  products2.forEach(item => {
+      const productItem = document.createElement("div");
+      productItem.setAttribute("class", "product-item");
+
+      const productItemHeader = document.createElement("div");
+      productItemHeader.setAttribute("class", "product-item--header");
+      const productImage = document.createElement("img");
+      productImage.setAttribute("src", item.imageUrl);
+      productImage.setAttribute("alt", item.name);
+      productItemHeader.appendChild(productImage);
+
+      const productItemDescDiv = document.createElement("div");
+      productItemDescDiv.setAttribute("class", "product-item--desc");
+
+      const productItemNameDiv = document.createElement("div");
+      productItemNameDiv.setAttribute("class", "product-item--name")
+      const productItemName = document.createElement("h2");
+      productItemName.textContent = item.name;
+      productItemNameDiv.appendChild(productItemName);
+
+      const productItemDetailDiv = document.createElement("div");
+      productItemDetailDiv.setAttribute("class", "product-item--details")
+      const productItemDetail = document.createElement("p");
+      productItemDetail.textContent = item.details;
+      productItemDetailDiv.appendChild(productItemDetail);
+
+      const productItemPriceDiv = document.createElement("div");
+      const productItemPrice = document.createElement("h3");
+      productItemPrice.setAttribute("class", "product-item--price")
+      productItemPrice.textContent = item.price + "$";
+      productItemPriceDiv.appendChild(productItemPrice);
+
+      const productItemBtnDiv = document.createElement("div");
+      productItemBtnDiv.setAttribute("class", "product-item-btn");
+      
+      const addProductToCartBtn = document.createElement("button");
+      addProductToCartBtn.setAttribute("class", "add-to-cart-btn");
+      addProductToCartBtn.textContent = "Add To Cart";
+      productItemBtnDiv.appendChild(addProductToCartBtn);
+
+      productItemDescDiv.appendChild(productItemNameDiv);
+      productItemDescDiv.appendChild(productItemDetailDiv);
+      productItemDescDiv.appendChild(productItemPrice);
+
+      productItem.appendChild(productItemHeader);
+      productItem.appendChild(productItemDescDiv);
+      productItem.appendChild(productItemBtnDiv);
+
+      productContainer.appendChild(productItem)
+  })
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,58 @@
-const products = [{}];
+const products = [
+  {
+    id: "p1",
+    name: "T-shirt",
+    imageUrl: "https://via.placeholder.com/150/255595/808080",
+    details: "",
+    price: 25,
+    category: "cat_1",
+  },
+  {
+    id: "p2",
+    name: "Shouse",
+    imageUrl: "https://via.placeholder.com/150/255595/808080",
+    details: "",
+    price: 15,
+    category: "cat_1",
+  },
+  {
+    id: "p3",
+    name: "Toy",
+    imageUrl: "https://via.placeholder.com/150/255595/808080",
+    details: "",
+    price: 5,
+    category: "cat_2",
+  },
+];
+
+const productSearch = function (text, products) {
+ return [];
+};
+
+const productSearchByPrice = function (text, products) {
+  return []
+};
+
+const filterProoduct = function (filteredBy) {
+  return [];
+};
+
+const addToCart = function (product, cart) {
+  return [];
+};
+
+const totalPrice = function (cart) {
+  return 0;
+};
+
+totalPrice(cart);
+
+module.exports = {
+  products,
+  productSearch,
+  filterProoduct,
+  addToCart,
+  totalPrice,
+  productSearchByPrice,
+  cart,
+};
